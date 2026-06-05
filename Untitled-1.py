4e57714c75edf7e1e201cffa5b5225568cc9ce81
ivan.cinho <pabloivan84@gmail.com>
2026-06-05 11:02:53 -0300

    5-6 commit inicial

---
+++ Untitled-1.py
@@ -0,0 +1,74 @@
+
+opcion = 0
+while opcion != 9: # bucle principal
+    opcion = input(print("Selecciones la opcion")) #menu principal
+
+    while not opcion.isdigit or int(opcion) < 1 or int(opcion) > 9:
+        print()
+        opcion = input(print("Selecciones la opcion")) #menu principal
+
+    opcion = int(opcion)
+
+    # Funciones auxiliares
+    # Actualizar datos
+    def agregar_pais():
+        print()
+    
+    def actualizar_datos():
+        print()
+
+
+    def buscar_pais():
+        print()
+
+    # Filtros
+    def filtrar_por_continente():
+        print()
+    
+    def filtrar_por_poblacion():
+        print()
+    
+    def filtrar_por_superficie():
+        print()
+    
+    # Ordenar
+    def ordenar_por_nombre():
+        print()
+
+    def ordenar_por_poblacion():
+        print()
+
+    def ordenar_por_superficie():
+        print()
+
+    # mostrar estadisticas
+    def mayor_y_menor_poblacion():
+        print()
+
+    def promedio_de_poblacion():
+        print()
+
+    def promedio_de_superficie():
+        print()
+
+    def paises_por_continente():
+        print()
+
+    # Programa
+    match opcion:
+        case 1:
+            print()
+        case 2:
+            print()
+        case 3:
+            print()
+        case 4:
+            print()
+        case 5:
+            print()
+        case 6:
+            print()
+        case 7:
+            print()
+        case 8:
+            print()
