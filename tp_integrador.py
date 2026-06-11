b351bfa7b8972fa03a33f78d33b5d5150cc41acb
ivan.cinho <pabloivan84@gmail.com>
2026-06-10 21:14:59 -0300

    Agrege la funcion agregar_pais y las funciones auxuliares validar_nombre y validar_numero_entero para el manejo de datos ingresados, modifique el case 1

--- tp_integrador.py
+++ tp_integrador.py
@@ -23,8 +23,13 @@ error_de_opcion = """"
 ========================================
 """
 
-# Funciones auxiliares
+mensaje_agregar_pais = """"
+========================================
+            AGREGAR PAIS
+========================================
+"""
 
+# Funciones auxiliares
 #Funcion para obtener los datos del .csv y acumularlos en una lista
 def cargar_datos(): 
     paises = []
@@ -43,10 +48,11 @@ def cargar_datos():
             paises.append(pais)
     return paises
 
+# Funcion para dar formato previo a ejecutar otras funciones evitando incluirla en ellas
 def formatear_datos(pais):
     return f"{pais['nombre']},{pais['poblacion']},{pais['superficie']},{pais['continente']}"
 
-#Funcion para mostrar/printear los datos en pantalla, invoca a la funcion cargar_datos()
+#Funcion para mostrar/printear los datos en pantalla, invoca a la funcion formatear_datos()
 def mostrar_datos(paises): 
     for pais in paises:
         print(formatear_datos(pais))
@@ -59,9 +65,90 @@ def guardar_datos(paises):
             listado.write(formatear_datos(pais) + "\n")
 
 # Actualizar datos
+#Funcion para validar que los datos numericos cumplen con el formato
+def validar_numero_entero(numero):
+    if not numero:
+        print("ERROR - No puede dejar el dato vacio")
+        return None
+    try:
+        numero = int(numero)
+        if numero <= 0:
+            print("El numero no puede ser menor a cero (0)")
+            return None
+        else:
+            return numero
+    except ValueError:
+        print("Debe ingresar un numero")   
+
+#Funcion para validar que el nombre cumpla con el formato
+def validar_nombre(nombre):
+    nombre = nombre.strip()
+    if not nombre:
+        print("ERROR - No puede dejar el dato vacio")
+        return None
+    if not nombre.replace(" ", "").isalpha():
+        print("ERROR - Solo puede ingresar texto")
+        return None
+    else:
+        nombre = nombre.title()
+        return nombre
+
+#Funccion para agregar un nuevo pais con sus 4 datos correspondientes, tal vez se pueda reducir o modularizar
 def agregar_pais():
-    print()
-    
+    print(mensaje_agregar_pais)
+    while True: #Bloque para el nombre
+        nombre = input("Ingrese el nombre del pais: ").strip()
+        nombre_valido = validar_nombre(nombre) #Invocacion a la funcion validar_nombre 
+        if not nombre_valido:
+            continue
+        paises = cargar_datos()
+        existe = False
+        for p in paises: #Verficacion de nombre duplicado
+            if p["nombre"].lower() == nombre_valido.lower():
+                print(f"ERROR - El pais {nombre_valido} ya existe en la lista")
+                existe = True
+                break 
+        if not existe:
+            nombre = nombre_valido
+            print("Nombre ingresado")
+            break
+
+    while True: #Bloque para la poblacion
+        poblacion_str = input("Ingrese la poblacion: ").strip()
+        poblacion_validada = validar_numero_entero(poblacion_str)
+        if poblacion_validada is not None:
+            poblacion = poblacion_validada
+            print("Poblaion ingresada")
+            break
+
+    while True: #Bloque para la superficie
+        superficie_str = input("Ingrese la superficie: ").strip()
+        superficie_validada = validar_numero_entero(superficie_str)
+        if superficie_validada is not None:
+            superficie = superficie_validada
+            print("Superficie ingresada")
+            break
+
+    while True: #Bloque para el continente
+        continente = input("Ingrese el continente: ").strip()
+        continente_validado = validar_nombre(continente)
+        if continente_validado:
+            continente = continente_validado
+            print("Continente ingresado")
+            break
+
+    paises.append({  # Agregar a la lista y guardar
+        "nombre": nombre,
+        "poblacion": poblacion,
+        "superficie": superficie,
+        "continente": continente
+    })
+    guardar_datos(paises)
+    print(f"\n'{nombre}' se agrego correctamente")
+    print("\n--- LISTA ACTUALIZADA ---")
+    mostrar_datos(paises)
+
+
 def actualizar_datos():
     print()
 
@@ -111,9 +198,10 @@ while True:
         else:
             match opcion:
                 case 1:
-                    mostrar_datos()
+                    paises = cargar_datos()
+                    mostrar_datos(paises)
                 case 2:
-                    print("Agregar un pais")
+                    agregar_pais()
                 case 3:
                     print("Actualizar datos de poblacion o superficie")
                 case 4:
